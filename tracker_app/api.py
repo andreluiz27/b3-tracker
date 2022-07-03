d62e11f237de7e5d6f13692c5207d59b0dd18286
Andre Luiz Ferreira da Silva Nogueira <aluizfs@avelar.aquarius.cpqd.com.br>
2022-07-03 17:53:29 -0300

    Writting tracker code

--- tracker_app/api.py
+++ tracker_app/api.py
@@ -9,13 +9,32 @@ from yahooquery import Ticker
 #     stock = Ticker("SÍMBOLO DA AÇÃO")
 #     while(True):
 #         stock.history
-    
+
 
 @shared_task()
 def stock_tracker():
-     stock = Ticker("SÍMBOLO DA AÇÃO")
-     stock.history(period="1d",  interval = "30m")
+
+    stock = Ticker("PETR4.SA")
+
+    # df, i.e, a dataframe type
+    stock_df_time_serie = stock.history(period="1d", interval="30m")
+
+    # get first row
+    stock_df_first_row = stock_df_time_serie.head(1)
+
+    # It's a multi index situation
+    symbol = stock_df_first_row.index[0][0]  # symbol it's a index
+    dt_time = stock_df_first_row.index[0][1]  # date it's a index
+
+    open_value = float(stock_df_first_row.open.values)
+    low_value = float(stock_df_first_row.low.values)
+    high_value = float(stock_df_first_row.high.values)
+    close_value = float(stock_df_first_row.close.values)
+    volume = int(stock_df_first_row.volume.values)
+    interval = "30m"
+
 
 petr = Ticker("PETR4.SA")
-a=petr.history(period="1d",  interval = "30m")
+a = petr.history(period="1d", interval="30m")
+
 print(a)
